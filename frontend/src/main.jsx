1e5250d310fa41ea13ecedb732dccd28569e0aee
Poonam219 <Poonamnimbalkar2003@gmail.com>
2025-10-18 17:39:52 +0530

    Initial commit to new repo

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./styles.css";
+
 import App from "./App.jsx";
 
 import ProductList from "./pages/ProductList.jsx";
